e5c27c94de0e6aca02ed7439fd29e0ad74387559
Eliasamani <eliasamanii@gmail.com>
2024-04-17 15:04:04 +0200

    Temporary styling

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -9,19 +9,25 @@ export default function LoginPage() {
     const router = useRouter();
     const handleGoogleLogin = async () => {
         try {
-            const user = await useGoogleLogin(); // Correct usage of the imported function
+            const user = await useGoogleLogin();
             console.log('Logged in user:', user);
-            // Redirect or manage state as needed, such as redirecting to the dashboard or home page
-            router.push('/dashboard');
+            router.push('/dashboard'); // Navigate to dashboard after login
         } catch (error) {
             console.error('Error logging in:', error);
         }
     };
 
     return (
-        <div>
-            <h1>Login to Pix-E</h1>
-            <button onClick={handleGoogleLogin}>Log in with Google</button>
+        <div className="flex min-h-screen items-center justify-center bg-gray-100">
+            <div className="p-8 bg-white shadow-md rounded-lg">
+                <h1 className="text-2xl font-bold text-gray-800 mb-6">Login to Pix-E</h1>
+                <button
+                    onClick={handleGoogleLogin}
+                    className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50"
+                >
+                    Log in with Google
+                </button>
+            </div>
         </div>
     );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,33 +1,26 @@
-//app/page.js
+// app/page.js
 import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
       <Head>
         <title>Pix-E | Pixel Art Display</title>
         <meta name="description" content="Display and create beautiful pixel art with Pix-E." />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <h1>
-          Welcome to Pix-E!
-        </h1>
-        <p>
-          Create and share your pixel art creations.
-        </p>
-
+      <main className="max-w-md w-full p-8 bg-white shadow-md rounded-lg text-center">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome to Pix-E!</h1>
+        <p className="text-lg text-gray-600 mb-6">Create and share your pixel art creations.</p>
         <div>
-          <a href="/auth">
-            <h2>Login &rarr;</h2>
+          <a href="/auth" className="text-blue-500 hover:text-blue-700">
+            <h2 className="text-lg font-semibold">Login &rarr;</h2>
           </a>
         </div>
       </main>
 
-      <footer>
-        <p>Pix-E</p>
-      </footer>
+      <footer className="mt-8 text-gray-500">Pix-E</footer>
     </div>
   );
 }
