f5c89a5b9e47c10ea6260fa709b8b034356a580d
fetost <patranikap@gmail.com>
2024-04-19 22:06:47 +0200

    Worked on the private gallery

--- src/app/privateGallery/myGalleryPresenter.jsx
+++ src/app/privateGallery/myGalleryPresenter.jsx
@@ -1,11 +1,46 @@
 'use client'
 
 import { observer } from "mobx-react-lite";
-
+import { useState, useEffect } from 'react';
 import MyGalleryView from "./myGalleryView.jsx";
+import {connectToFirebase, saveToFirebase, readFromFirebase} from "@/firebaseModel.js";
 
-export default observer(
-    function MyGallery(){
-        return <MyGalleryView/>
-    }
-);
+function MyGallery() {
+  const [model, setModel] = useState({
+    ready: false,
+    testPicture: null,
+    pictures: [],
+  });
+
+  useEffect(() => {
+    connectToFirebase(model);
+  }, []);
+
+const savePicture = () => {
+    // Update the model state using setModel
+    setModel(prevModel => {
+        const updatedModel = {
+            ...prevModel,
+            testPicture: prevModel.testPicture
+        };
+
+        // Save the updated model to Firebase
+        saveToFirebase(updatedModel);
+
+        return updatedModel;
+    });
+}
+
+const loadGallery = () => {
+    readFromFirebase(model).then(data => {
+        setModel(prevModel => ({
+            ...prevModel,
+            pictures: data,
+        }));
+    });
+}
+
+return <MyGalleryView savePicture={savePicture} loadGallery={loadGallery} pictures={model.pictures} />
+}
+
+export default observer(MyGallery);
--- src/app/privateGallery/myGalleryView.jsx
+++ src/app/privateGallery/myGalleryView.jsx
@@ -1,7 +1,12 @@
-export default function myGalleryView() {
+export default function MyGalleryView({ savePicture, loadGallery, pictures }) {
     return (
         <div>
             <h1>Gallery</h1>
+            <button onClick={savePicture}>Save Picture</button>
+            <button onClick={loadGallery}>Load Gallery</button>
+            {pictures.map((picture, index) => (
+                <img key={index} src={picture} alt="User pic" />
+            ))}
             <p>Blablabla</p>
         </div>
     );
