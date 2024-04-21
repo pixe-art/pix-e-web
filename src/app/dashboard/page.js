f2c5d79906d792a8bfb2699ad4da43712893432a
Eliasamani <eliasamanii@gmail.com>
2024-04-22 00:10:02 +0200

    Added "Currently on your Pix-E" placeholder

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,25 +1,44 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 export default function Dashboard() {
-    return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        <div className="max-w-md w-full px-8 py-6 bg-white shadow-md rounded-lg text-center">
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">Welcome to Pix-E Dashboard!</h1>
-          <p className="text-lg text-gray-600 mb-6">Create and share your pixel art creations.</p>
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full px-8 py-6 bg-white shadow-md rounded-lg text-center border-8 border-black rounded-xl">
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Welcome to Pix-E Dashboard!
+        </h1>
+        <p className="text-lg text-gray-600 mb-6">
+          Create and share your pixel art creations.
+        </p>
+        <h2 className="text-lg font-bold text-gray-800 mb-4">Currently on your Pix-E:</h2>
 
-          <div className="flex flex-wrap gap-4 justify-center mb-6">
-            <Link href="/art-tool">
-              <span className="inline-block px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 cursor-pointer">Art Tool</span>
-            </Link>
-            <Link href="/privateGallery">
-              <span className="inline-block px-6 py-2 text-white bg-green-500 rounded hover:bg-green-600 cursor-pointer">Private Gallery</span>
-            </Link>
-            <Link href="/publicGallery">
-              <span className="inline-block px-6 py-2 text-white bg-red-500 rounded hover:bg-red-600 cursor-pointer">Public Gallery</span>
-            </Link>
-          </div>
+        <div className="mb-6 bg-black w-320 h-640 border-4 border-black rounded-xl">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/pix-e-b9fab.appspot.com/o/art1.png?alt=media&token=afd1e2a8-0da2-4524-a0b4-7f813fffd7d1"
+            alt="Pixel Art"
+            className="w-full h-full object-contain rounded-xl"
+          />
         </div>
-        <footer className="mt-8 text-gray-500">Pix-E</footer>
+
+        <div className="flex flex-wrap gap-4 justify-center mb-6">
+          <Link href="/art-tool">
+            <span className="inline-block px-6 py-2 text-white bg-blue-500 rounded hover:bg-blue-600 cursor-pointer">
+              Art Tool
+            </span>
+          </Link>
+          <Link href="/privateGallery">
+            <span className="inline-block px-6 py-2 text-white bg-green-500 rounded hover:bg-green-600 cursor-pointer">
+              Private Gallery
+            </span>
+          </Link>
+          <Link href="/publicGallery">
+            <span className="inline-block px-6 py-2 text-white bg-red-500 rounded hover:bg-red-600 cursor-pointer">
+              Public Gallery
+            </span>
+          </Link>
+        </div>
       </div>
-    );
-  }
+      <footer className="mt-8 text-gray-500">Pix-E</footer>
+    </div>
+  );
+}
