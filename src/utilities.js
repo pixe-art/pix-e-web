a36188e6df2435cfac6cebb734e342ed9f38c373
FryFish4U <126065213+FryFish4U@users.noreply.github.com>
2024-04-21 13:36:37 +0200

    bugfix for different screen scaling

--- src/utilities.js
+++ src/utilities.js
@@ -2,13 +2,12 @@ export function getCords(element, x, y, offset) {
     //* assign values to variables
     const style = getComputedStyle(element);
     const canvas = element.getBoundingClientRect();
-    const offsetBorder = parseInt(style.border.split("px")[0]);
-    const offsetPadding = parseInt(style.padding.split("px")[0]);
-    const fullOffset = Math.floor(offset + offsetBorder + offsetPadding);
-
+    const offsetBorder = (style.border.split("px")[0]);
+    const offsetPadding = (style.padding.split("px")[0]);
+    const fullOffset = Math.round(offset + offsetBorder + offsetPadding);
     //* calculate relative positions
-    const posX = (Math.floor(((x - canvas.left)/style.scale)) - fullOffset);
-    const posY = (Math.floor(((y - canvas.top)/style.scale)) - fullOffset);
+    const posX = (Math.round(((x - (canvas.left))/(style.scale))) - fullOffset);
+    const posY = (Math.round(((y - (canvas.top))/(style.scale))) - fullOffset);
 
     return [posX, posY];
 }
