747132561a0a3b5161527eb92a878a7411bf555a
FryFish4U <126065213+FryFish4U@users.noreply.github.com>
2024-04-24 15:27:22 +0200

    Moved art tool functions from view to presenter

--- src/app/art-tool/presenter.jsx
+++ src/app/art-tool/presenter.jsx
@@ -3,12 +3,100 @@
 'use client'; 
 import ArtTool from "./view";
 import { observer } from "mobx-react-lite"; //? observer
+import React, { useState } from "react";
 
 export default observer(
     function Tool() {
-            
+        let mouseCheck = false;
+        let lastXY = new Array(2);
+        let penSize = 1;
+        let penColor = "black";
+        let drawColor = "black";
+        const historyLength = 10
+        let undoHistory = new Array(historyLength);
+    
+        function printDebugInfo(canvas) {
+            const foo = canvas.getBoundingClientRect();
+            const style = getComputedStyle(canvas);
+            const multH = foo.height/canvas.height;
+            const multW = foo.width/canvas.width;
+            console.log("\n[ DEBUG INFO ]\n");
+            console.log("canvas scaling = " + style.scale);
+            console.log("size difference (mult):\nwidth = " + multW + "x\nheight = " + multH + "x");
+            console.log("canvas size:\nwidth = " + foo.width + "\nheight = " + foo.height);
+            console.log("canvas edges:\nlft = " + foo.left + "\nrgt = " + foo.right + "\ntop = " + foo.top + "\nbot = " + foo.bottom);
+            console.log("border = " + style.border + "\n(needs offset of " + style.border.split(" ")[0] + ")");
+            console.log("padding = " + style.padding + "\n(needs offset of " + style.padding.split(" ")[0] + ")");
+        }
+    
+        let [color, setColor] = useState("#000000"); // default color (black)
+        let [showPicker, setShowPicker] = useState(false)
+
+        function checkReset() {
+            //* flags check as false, used for mouseDragEvent
+            mouseCheck = false;
+        }
+        function clearUndoHistory(){
+            console.log("clearing history");
+            undoHistory = new Array(historyLength)
+        }
+        function unshiftUndoHistory(canvas) {
+            if (undoHistory.at(historyLength)) {
+                undoHistory.pop()
+            }
+            undoHistory.unshift(canvas.toDataURL())
+        }
+        function grabLastImage() {
+            const last = undoHistory[0];
+            undoHistory = undoHistory.slice(1)
+            return last;
+        }
+        function currentColor(reset) {
+            if (reset) {
+                drawColor = color
+            }
+            return drawColor
+        }
+        function changePenSize(size){
+            if (size) {
+                penSize = size 
+            }
+            return penSize;
+        }
+        function setLastXY(xy) {
+            if (xy) {
+                lastXY = xy;                
+            }
+            return lastXY;
+        }
+        
+
+        function handleColorChange(color){
+            setColor(color.hex);
+            drawColor = color.hex;
+        }
+
         return(
-            <ArtTool/>
+            <ArtTool
+                showPicker = {showPicker}
+                mouseCheck = {mouseCheck}
+                lastXY = {lastXY}
+                penSize = {penSize}
+                penColor = {penColor}
+                drawColor = {drawColor}
+                undoHistory = {undoHistory}
+                color = {color}
+                setShowPicker = {setShowPicker}
+                checkReset = {checkReset}
+                grabLastImage = {grabLastImage}
+                unshiftUndoHistory = {unshiftUndoHistory}
+                changePenSize = {changePenSize}
+                setLastXY = {setLastXY}
+                clearUndoHistory = {clearUndoHistory}
+                printDebugInfo = {printDebugInfo}
+                handleColorChange = {handleColorChange}
+                currentColor = {currentColor}
+            />
         );
     }
 )
--- src/app/art-tool/view.jsx
+++ src/app/art-tool/view.jsx
@@ -7,19 +7,15 @@ import React,{useState} from "react";
 import { SketchPicker } from 'react-color';
 import Draggable from './draggable';
 
-function ArtTool() {
-const [color, setColor] = useState("#000000"); // default color (black)
-const [showPicker, setShowPicker] = useState(false)
+//! temporary var:
+let fresh = true;
 
-const handleColorChange = (color) => {
-    setColor(color.hex);
-    penColor = color.hex;
-}
-const paletteButtonClick = () => {
-    setShowPicker(!showPicker);
-}
+function ArtTool(props) {
+    const paletteButtonClick = () => {
+        props.setShowPicker(!props.showPicker);
+    }
     return( 
-        <div className="parent" onMouseUp={checkReset}>
+        <div className="parent" onMouseUp={props.checkReset}>
             <div className="topbar">
                 <h1>Left-Click to draw | Right-Click to erase | Middle-Click for single pixel</h1>
                 <button onClick={debugEvent} type="button">click for debug info</button>
@@ -28,9 +24,9 @@ const paletteButtonClick = () => {
                 <div className="palette">
                 <Draggable className="draggable">
                     <button className="palette-button" onClick={paletteButtonClick}>Toggle Color Palette</button>
-                        {showPicker && (
+                        {props.showPicker && (
                             <div className="color-palette">
-                                <SketchPicker color={color} onChangeComplete={handleColorChange} />
+                                <SketchPicker color={props.color} onChangeComplete={props.handleColorChange} />
                             </div>
                             )}
                 </Draggable>
@@ -54,245 +50,218 @@ const paletteButtonClick = () => {
                             <p>&nbsp;</p>                            
                             <p id="pen-size-d">1</p>                            
                         </div>
-                        <input type="range" name="pen-size" id="pen-size" defaultValue={"1"} min={"1"} max={"12"} onChange={penSizeEvent}/>
+                        <input type="range" name="pen-size" id="pen-size" defaultValue={props.changePenSize()} min={"1"} max={"12"} onChange={penSizeEvent}/>
                     </div>
                 </div>
             </div>
 
         </div>
     );
-}
-export default ArtTool;
 
-
-//global variables
-let mouseCheck = false;
-let lastXY;
-let penSize = 1;
-let penColor = "black";
-let currentColor = "black";
-const historyLength = 10
-let undoHistory = new Array(historyLength);
-
-let fresh = true;
-
-function debugEvent() {
-    //! log outputs for checking canvas size
-    const element = document.getElementById("drawing-area");
-    const foo = element.getBoundingClientRect();
-    const style = getComputedStyle(element);
-    const multH = foo.height/element.height;
-    const multW = foo.width/element.width;
-    console.log("\n[ DEBUG INFO ]\n");
-    console.log("canvas scaling = " + style.scale);
-    console.log("size difference (mult):\nwidth = " + multW + "x\nheight = " + multH + "x");
-    console.log("canvas size:\nwidth = " + foo.width + "\nheight = " + foo.height);
-    console.log("canvas edges:\nlft = " + foo.left + "\nrgt = " + foo.right + "\ntop = " + foo.top + "\nbot = " + foo.bottom);
-    console.log("border = " + style.border + "\n(needs offset of " + style.border.split(" ")[0] + ")");
-    console.log("padding = " + style.padding + "\n(needs offset of " + style.padding.split(" ")[0] + ")");
-}
-
-function clearCanvas() {
-    try {
-        // declare elements
+    function debugEvent() {
+        //! log outputs for checking canvas size
         const element = document.getElementById("drawing-area");
-        const extra = element.getContext("2d");
-        // fill canvas with white
-        extra.save();
-        extra.fillStyle = "white";
-        extra.fillRect(0,0, element.width, element.height);        
-        extra.restore();
-        // reset undo history
-        undoHistory = new Array(historyLength);
-    } catch (error) {
-        console.error(error);
+        props.printDebugInfo(element)
     }
-}
-function checkReset() {
-    //* flags check as false, used for mouseDragEvent
-    mouseCheck = false;
-}
-function downloadCanvas() {
-    const element = document.getElementById("drawing-area");
-    const temp = document.createElement('a');
-    const img = element.toDataURL("image/png").replace("image/png", "image/octet-stream");
-    temp.setAttribute("href", img);
-    temp.setAttribute("download", "canvas.png");
-    temp.click();   
-    temp.remove();
-}
-function saveCurrent() {
-    const element = document.getElementById("drawing-area");
-    if (undoHistory.at(historyLength)) {
-        undoHistory.pop()
-    }        
-    undoHistory.unshift(element.toDataURL())
-}
-function undo() {
-    const element = document.getElementById("drawing-area");
-    let img = new Image()
-    let last = undoHistory[0];
-    undoHistory = undoHistory.slice(1)
-    if (!last) {
-        return;
+    function clearCanvas() {
+        console.log("clearing canvas");
+        try {
+            // declare elements
+            const element = document.getElementById("drawing-area");
+            const extra = element.getContext("2d");
+            // fill canvas with white
+            extra.save();
+            extra.fillStyle = "white";
+            extra.fillRect(0,0, element.width, element.height);        
+            extra.restore();
+            // reset undo history
+            props.clearUndoHistory();
+        } catch (error) {
+            console.error(error);
+        }
     }
-    img.src = last;
-    img.onload = () => {
-        element.getContext("2d").drawImage(img,0,0);
+    function downloadCanvas() {
+        const element = document.getElementById("drawing-area");
+        const temp = document.createElement('a');
+        const img = element.toDataURL("image/png").replace("image/png", "image/octet-stream");
+        temp.setAttribute("href", img);
+        temp.setAttribute("download", "canvas.png");
+        temp.click();   
+        temp.remove();
     }
-}
-function eraserEvent(event) {
-    const element = document.getElementById(event.target.id);
-    if (element.className.includes("active")) {
-        element.className = ""
-        currentColor = penColor;
-    } else {
-        element.className = "active"
-        currentColor = "white"
+    function saveCurrent() {
+        const canvas = document.getElementById("drawing-area");
+        props.unshiftUndoHistory(canvas)     
     }
-}
-function penSizeEvent(event) {
-    penSize = event.target.value;
-    document.getElementById("pen-size-d").innerHTML = event.target.value;
-}
-
-function resetLastCoords(){
-    lastXY = [-1, -1];
-}
-function mouseClickEvent(event) {
-    if (fresh) {
-        fresh = false;
-        clearCanvas();
+    function undo() {
+        const element = document.getElementById("drawing-area");
+        let img = new Image()
+        const last = props.grabLastImage()
+        if (!last) {
+            return;
+        }
+        img.src = last;
+        img.onload = () => {
+            element.getContext("2d").drawImage(img,0,0);
+            img.remove();
+        }
     }
-    if (penColor !== currentColor && penColor !== "white") {
-        currentColor = penColor
-    }
-    event.preventDefault();
-    saveCurrent();
-    mouseCheck = true //for mouseDragEvent()
-    const element = document.getElementById(event.target.id);
-    //* translate event coordiantes to the canvas 
-    const cords = getCords(element, event.clientX, event.clientY, (penSize-1)/2);
-
-    //* draw pixel
-    const extra = element.getContext("2d");
-    extra.save();
-    if (event.button === 2) {
-        extra.fillStyle = "white";
-    } else {
-        extra.fillStyle = currentColor;
-    }
-    extra.fillRect(cords[0], cords[1], penSize, penSize); //draw pixel at translated coordinates
-    lastXY = cords; //used for fallback in mouseDragEvent()
-    extra.restore();
-    }
-function mouseDragEvent(event) {
-    if ((event.buttons === 2 || event.buttons === 1) && mouseCheck) {
+    function eraserEvent(event) {
         const element = document.getElementById(event.target.id);
-        const extra = element.getContext("2d");
-        const cords = getCords(element, event.clientX, event.clientY, (penSize-1)/2);
+        if (element.className.includes("active")) {
+            element.className = ""
+            props.currentColor(true);
+        } else {
+            element.className = "active"
+            props.drawColor = "white"
+        }
+    }
+    function penSizeEvent(event) {
+        document.getElementById("pen-size-d").innerHTML = props.changePenSize(event.target.value);
+    }
 
+    function resetLastCoords(){
+        props.setLastXY([-1, -1]);
+    }
+    function mouseClickEvent(event) {
+        if (fresh) {
+            fresh = false;
+            clearCanvas();
+        }
+        if (props.penColor !== props.drawColor && props.penColor !== "white") {
+            props.currentColor(true)
+        }
+        event.preventDefault();
+        saveCurrent();
+        const element = document.getElementById(event.target.id);
+        //* translate event coordiantes to the canvas 
+        const cords = getCords(element, event.clientX, event.clientY, (props.changePenSize()-1)/2);
+
+        //* draw pixel
+        const extra = element.getContext("2d");
         extra.save();
-        // set draw color to white for erase if right click
-        extra.fillStyle = "white";
-        if (event.buttons === 2) {
+        if (event.button === 2) {
             extra.fillStyle = "white";
         } else {
-            extra.fillStyle = currentColor;
+            extra.fillStyle = props.drawColor;
         }
-        if (lastXY[0] < 0 || lastXY[1] < 0) {
-            lastXY = cords;
-        }
-        draw_line(lastXY[0], lastXY[1], cords[0], cords[1], extra);
+        extra.fillRect(cords[0], cords[1], props.changePenSize(), props.changePenSize()); //draw pixel at translated coordinates
+        props.setLastXY(cords); //used for fallback in mouseDragEvent()
         extra.restore();
+        }
+    function mouseDragEvent(event) {
+        if ((event.buttons === 2 || event.buttons === 1)) {
+            const element = document.getElementById(event.target.id);
+            const extra = element.getContext("2d");
+            const cords = getCords(element, event.clientX, event.clientY, (props.changePenSize()-1)/2);
 
-        lastXY = cords;
-    }
-}
-function touchDrawEvent(event){
-    const element = document.getElementById(event.target.id);
-    const cords = getCords(element, event.targetTouches[0]?.clientX || event.clientX, event.targetTouches[0]?.clientY || event.clientY)
-    element.getContext("2d").fillRect(cords[0], cords[1], penSize, penSize)
-    lastXY = cords;
-}
-function touchDragEvent(event) {
-    const element = document.getElementById(event.target.id);
-    const extra = element.getContext("2d");
-    try {
-        const cords = getCords(element, 
-            event.targetTouches[0]?.clientX || event.Touch[0]?.clientX || event.clientX, 
-            event.targetTouches[0]?.clientY || event.Touch[0]?.clientY || event.clientY);            
-            draw_line(lastXY[0], lastXY[1], cords[0], cords[1], extra);
-            lastXY = cords;
-    } catch (error) {
-        return;
-    }
-}
-// draw_line taken and adapted from https://ghost-together.medium.com/how-to-code-your-first-algorithm-draw-a-line-ca121f9a1395
-function draw_line(x1, y1, x2, y2, con) {
-    // Iterators, counters required by algorithm
-    let x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
-    // Calculate line deltas
-    dx = x2 - x1;
-    dy = y2 - y1;
-    // Create a positive copy of deltas (makes iterating easier)
-    dx1 = Math.abs(dx);
-    dy1 = Math.abs(dy);
-    // Calculate error intervals for both axis
-    px = 2 * dy1 - dx1;
-    py = 2 * dx1 - dy1;
-    
-    if (dy1 <= dx1) { //* The line is X-axis dominant
-        // Line is drawn left to right
-        if (dx >= 0) {
-            x = x1; y = y1; xe = x2;
-        } else { // Line is drawn right to left (swap ends)
-            x = x2; y = y2; xe = x1;
-        }
-        con.fillRect(x, y, penSize, penSize); // draws pixel at translated coordinates
-        // Rasterize the line
-        for (i = 0; x < xe; i++) {
-            x = x + 1;
-            // Deal with octants...
-            if (px < 0) {
-                px = px + 2 * dy1;
+            extra.save();
+            // set draw color to white for erase if right click
+            extra.fillStyle = "white";
+            if (event.buttons === 2) {
+                extra.fillStyle = "white";
             } else {
-                if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
-                    y++;
-                } else {
-                    y--;
-                }
-                px = px + 2 * (dy1 - dx1);
+                extra.fillStyle = props.currentColor(true);
             }
-            // Draw pixel from line span at
-            // currently rasterized position
-            con.fillRect(x, y, penSize, penSize);
-        }
-    } else { //* The line is Y-axis dominant
-        // Line is drawn bottom to top
-        if (dy >= 0) {
-            x = x1; y = y1; ye = y2;
-        } else { // Line is drawn top to bottom
-            x = x2; y = y2; ye = y1;
-        }
-        con.fillRect(x, y, penSize, penSize); // draws pixel at translated coordinates
-        // Rasterize the line
-        for (i = 0; y < ye; i++) {
-            y++;
-            // Deal with octants...
-            if (py <= 0) {
-                py = px + 2 * dx1;
-            } else {
-                if ((dx < 0 && dy<0) || (dx > 0 && dy > 0)) {
-                    x++;
-                } else {
-                    x--;
-                }
-                py = py + 2 * (dx1 - dy1);
+            if (props.setLastXY()[0] < 0 || props.setLastXY()[1] < 0) {
+                props.setLastXY(cords);
             }
-            // Draw pixel from line span at
-            // currently rasterized position
-            con.fillRect(x, y, penSize, penSize); // draws pixel at translated coordinates
+            draw_line(cords[0],cords[1],extra)
+            extra.restore();
+            props.setLastXY(cords);
         }
     }
- }
+    function touchDrawEvent(event){
+        const element = document.getElementById(event.target.id);
+        const cords = getCords(element, event.targetTouches[0]?.clientX || event.clientX, event.targetTouches[0]?.clientY || event.clientY)
+        element.getContext("2d").fillRect(cords[0], cords[1], props.changePenSize(), props.changePenSize())
+        props.setLastXY(cords);
+    }
+    function touchDragEvent(event) {
+        const element = document.getElementById(event.target.id);
+        const extra = element.getContext("2d");
+        try {
+            const cords = getCords(element, 
+                event.targetTouches[0]?.clientX || event.Touch[0]?.clientX || event.clientX, 
+                event.targetTouches[0]?.clientY || event.Touch[0]?.clientY || event.clientY);            
+                // draw_line(props.lastXY[0], props.lastXY[1], , extra);
+                draw_line(cords[0],cords[1],extra)
+                props.setLastXY(cords);
+        } catch (error) {
+            
+        }
+    }
+    function draw_line(x2, y2, con) {
+        const x1 = props.setLastXY()[0]
+        const y1 = props.setLastXY()[1]
+        // draw_line taken and adapted from https://ghost-together.medium.com/how-to-code-your-first-algorithm-draw-a-line-ca121f9a1395
+        // Iterators, counters required by algorithm
+        let x, y, dx, dy, dx1, dy1, px, py, xe, ye, i;
+        // Calculate line deltas
+        dx = x2 - x1;
+        dy = y2 - y1;
+        // Create a positive copy of deltas (makes iterating easier)
+        dx1 = Math.abs(dx);
+        dy1 = Math.abs(dy);
+        // Calculate error intervals for both axis
+        px = 2 * dy1 - dx1;
+        py = 2 * dx1 - dy1;
+        
+        if (dy1 <= dx1) { //* The line is X-axis dominant
+            // Line is drawn left to right
+            if (dx >= 0) {
+                x = x1; y = y1; xe = x2;
+            } else { // Line is drawn right to left (swap ends)
+                x = x2; y = y2; xe = x1;
+            }
+            con.fillRect(x, y, props.changePenSize(), props.changePenSize());
+
+            // Rasterize the line
+            for (i = 0; x < xe; i++) {
+                x = x + 1;
+                // Deal with octants...
+                if (px < 0) {
+                    px = px + 2 * dy1;
+                } else {
+                    if ((dx < 0 && dy < 0) || (dx > 0 && dy > 0)) {
+                        y++;
+                    } else {
+                        y--;
+                    }
+                    px = px + 2 * (dy1 - dx1);
+                }
+                // Draw pixel from line span at
+                // currently rasterized position
+                con.fillRect(x, y, props.changePenSize(), props.changePenSize());
+            }
+        } else { //* The line is Y-axis dominant
+            // Line is drawn bottom to top
+            if (dy >= 0) {
+                x = x1; y = y1; ye = y2;
+            } else { // Line is drawn top to bottom
+                x = x2; y = y2; ye = y1;
+            }
+            con.fillRect(x, y, props.changePenSize(), props.changePenSize());
+
+            // Rasterize the line
+            for (i = 0; y < ye; i++) {
+                y++;
+                // Deal with octants...
+                if (py <= 0) {
+                    py = px + 2 * dx1;
+                } else {
+                    if ((dx < 0 && dy<0) || (dx > 0 && dy > 0)) {
+                        x++;
+                    } else {
+                        x--;
+                    }
+                    py = py + 2 * (dx1 - dy1);
+                }
+                // Draw pixel from line span at
+                // currently rasterized position
+                con.fillRect(x, y, props.changePenSize(), props.changePenSize());
+            }
+        }
+    } 
+}
+export default ArtTool;
