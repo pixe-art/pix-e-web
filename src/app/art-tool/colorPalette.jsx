ffd909dc0ae2d20b1d7258f8ff74fa8ba6785212
FryFish4U <126065213+FryFish4U@users.noreply.github.com>
2024-04-25 10:36:31 +0200

    stopped error spam from color palette

--- src/app/art-tool/colorPalette.jsx
+++ src/app/art-tool/colorPalette.jsx
@@ -1,9 +1,9 @@
 import React, {useState} from 'react';
 import { SketchPicker } from 'react-color';
 
-
-const ColorPalette = ({color = "black", onChangeComplete}) => (
-    <SketchPicker color = {color} onChangeComplete = {onChangeComplete} />
+//? ' color = "" ' stops log to be spammed with errors
+const ColorPalette = ({color, onChangeComplete}) => (
+    <SketchPicker color = {color = ""} onChangeComplete = {onChangeComplete} />
 );
 
 export default ColorPalette;
