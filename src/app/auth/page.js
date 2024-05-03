c910ae1595fef434b054b9b1f3c6fa43426282fd
Eliasamani <eliasamanii@gmail.com>
2024-05-03 14:57:22 +0200

    added colorCurrent variable to database

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -36,6 +36,7 @@ export default function LoginPage() {
             console.log(`Initializing data for UID: ${uid}...`);
             await set(userRef, {
                 device: 0,
+                colorCurrent: "",
                 profile: {
                     username: "",
                     bio: "",
